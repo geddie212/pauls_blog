b03548e8d3fca4ed6b4609e976514b8044e841ed
geddie212 <pauliusgedrimas@gmail.com>
2022-02-10 00:08:23 +0000

    change host main.py

--- main.py
+++ main.py
@@ -324,11 +324,11 @@ def login():
     return render_template('login.html', form=form)
 
 
-if __name__ == "__main__":
-    from app.main import app
-    import os
+# if __name__ == "__main__":
+#     from app.main import app
+#     import os
 
-    port = int(os.environ.get("PORT", 5000))
-    if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=port, debug=True)
+port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port, debug=True)
 
