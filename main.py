0aa29727af6df4d6296d905c65170ecba1df777c
geddie212 <pauliusgedrimas@gmail.com>
2022-02-10 00:22:01 +0000

    Procfile change

--- main.py
+++ main.py
@@ -329,7 +329,6 @@ def login():
 #     from app.main import app
 #     import os
 
-PORT = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(debug=True)
 
