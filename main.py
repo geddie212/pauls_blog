e77b005b57b057d3ff2081d8f93665f472b47d2a
geddie212 <pauliusgedrimas@gmail.com>
2022-03-08 00:56:53 +0000

    updated environment commands

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 # CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -36,7 +36,7 @@ login_manager.init_app(app)
 bootstrap = Bootstrap(app)
 
 # SECRET KEY SETUP
-app.config["SECRET_KEY"] = os.environ('SECRET_KEY')
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
 # CK EDITOR SETUP
 ckeditor = CKEditor(app)
